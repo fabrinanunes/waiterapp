e36d89c2a2ab0f316e83978dfc45b2bec92a1b5d
Fabrina <fabrina.nunes@hotmail.com>
2023-03-24 12:23:47 -0300

    feat: create index use cases

---
+++ api/src/app/useCases/index.ts
@@ -0,0 +1,11 @@
+import { createCategory } from './categories/createCategory';
+import { listCategories } from './categories/listCategories';
+import { createProduct } from './products/createProduct';
+import { listProducts } from './products/listProducts';
+import { listProductsByCategory } from './categories/listProductsByCategory';
+import { listOrders } from './orders/listOrders';
+import { createOrder } from './orders/createOrder';
+import { changeOrderStatus } from './orders/changeOrderStatus'
+import { cancelOrder } from './orders/cancelOrder';
+
+export { createCategory, listCategories, createProduct, listProducts, listProductsByCategory, listOrders, createOrder, changeOrderStatus, cancelOrder }
